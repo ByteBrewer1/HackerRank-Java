68f42e8ac21344012ccb429073d010e983ebf444
Rahul Mistry <22cs042@charusat.edu.in>
2023-07-16 12:16:29 +0530

    LOOP 2

--- Java Loops II/Solution.java
+++ Java Loops II/Solution.java
@@ -1,13 +1,27 @@
+// AUTHOR :: RAHUL MISTRY
+// DATE   :: 16 / 07 / 2023
+
 import java.io.*;
 import java.util.*;
+import java.lang.Math;
 
 public class Solution {
+    public static void main(String[] arg) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
 
-    public static void main(String[] args) {
-        /*
-         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
-         * class should be named Solution.
-         */
+        for (int i = 0; i < t; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int n = sc.nextInt();
 
+            for (int j = 0; j < n; j++) {
+                a += Math.pow(2, j) * b;
+                System.out.print(a + " ");
+            }
+            System.out.println();
+
+        }
+        sc.close();
     }
 }
